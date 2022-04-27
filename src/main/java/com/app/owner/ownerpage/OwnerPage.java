00d350b501d03292840c5178f7cc3e94132e7e19
Abhayda007 <54989146+Abhayda007@users.noreply.github.com>
2022-04-27 09:08:39 +0530

    Add files via upload

---
+++ src/main/java/com/app/owner/ownerpage/OwnerPage.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.app.owner.ownerpage;
+
+/**
+ *
+ * @author abhay
+ */
+public class OwnerPage {
+    public static void main(String[] args){
+        
+        owner owner = new owner();
+        //owner.setVisible(true);
+        
+        
+        new loginform().setVisible(true);
+        
+        
+    }
+}
